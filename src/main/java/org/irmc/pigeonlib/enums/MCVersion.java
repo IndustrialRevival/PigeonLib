a175680d79aa5d424799eb3691fea71df0815294
lijinhong11 <tygfhk@outlook.com>
2024-12-20 20:11:53 +0800

    edit

--- src/main/java/org/irmc/pigeonlib/enums/MCVersion.java
+++ src/main/java/org/irmc/pigeonlib/enums/MCVersion.java
@@ -11,34 +11,6 @@ import lombok.Getter;
 @SuppressWarnings("unused")
 @Getter
 public enum MCVersion {
-    MINECRAFT_1_16(16, 0),
-    MINECRAFT_1_16_1(16, 1),
-    MINECRAFT_1_16_2(16, 2),
-    MINECRAFT_1_16_3(16, 3),
-    MINECRAFT_1_16_4(16, 4),
-    MINECRAFT_1_16_5(16, 5),
-    MINECRAFT_1_17(17, 0),
-    MINECRAFT_1_17_1(17, 1),
-    MINECRAFT_1_17_2(17, 2),
-    MINECRAFT_1_17_3(17, 3),
-    MINECRAFT_1_17_4(17, 4),
-    MINECRAFT_1_18(18, 0),
-    MINECRAFT_1_18_1(18, 1),
-    MINECRAFT_1_18_2(18, 2),
-    MINECRAFT_1_18_3(18, 3),
-    MINECRAFT_1_18_4(18, 4),
-    MINECRAFT_1_19(19, 0),
-    MINECRAFT_1_19_1(19, 1),
-    MINECRAFT_1_19_2(19, 2),
-    MINECRAFT_1_19_3(19, 3),
-    MINECRAFT_1_19_4(19, 4),
-    MINECRAFT_1_20(20, 0),
-    MINECRAFT_1_20_1(20, 1),
-    MINECRAFT_1_20_2(20, 2),
-    MINECRAFT_1_20_3(20, 3),
-    MINECRAFT_1_20_4(20, 4),
-    MINECRAFT_1_20_5(20, 5),
-    MINECRAFT_1_20_6(20, 6),
     MINECRAFT_1_21(21, 0),
     MINECRAFT_1_21_1(21, 1),
     MINECRAFT_1_21_2(21, 2),
@@ -96,6 +68,10 @@ public enum MCVersion {
 
     public boolean equals(int major, int minor) {
         return this.major == major && this.minor == minor;
+    }
+
+    public boolean equals(MCVersion version) {
+        return this.major == version.major && this.minor == version.minor;
     }
 
     public boolean isUnknown() {
