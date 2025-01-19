ad20e303aca74c51ba64d97652d8cc97acbd84af
lijinhong11 <tygfhk@outlook.com>
2025-01-19 14:28:31 +0800

    0.0.6

--- src/main/java/org/irmc/pigeonlib/objects/percentage/PositiveHundredPercentage.java
+++ src/main/java/org/irmc/pigeonlib/objects/percentage/PositiveHundredPercentage.java
@@ -5,6 +5,55 @@ public final class PositiveHundredPercentage extends HundredPercentage{
         super(value, flowValidated);
     }
 
+    public static PositiveHundredPercentage fromIntPercentage(int value) {
+        return new PositiveHundredPercentage(value / 100.0, true);
+    }
+
+    public static PositiveHundredPercentage fromIntPercentage(int value, boolean flowValidated) {
+        return new PositiveHundredPercentage(value / 100.0, flowValidated);
+    }
+
+    public static PositiveHundredPercentage fromIntDecimal(int value) {
+        return new PositiveHundredPercentage(value, true);
+    }
+
+    public static PositiveHundredPercentage fromIntDecimal(int value, boolean flowValidated) {
+        return new PositiveHundredPercentage(value, flowValidated);
+    }
+
+    public static PositiveHundredPercentage fromFloatPercentage(float value) {
+        return new PositiveHundredPercentage(value / 100.0, true);
+    }
+
+    public static PositiveHundredPercentage fromFloatPercentage(float value, boolean flowValidated) {
+        return new PositiveHundredPercentage(value / 100.0, flowValidated);
+    }
+
+    public static PositiveHundredPercentage fromFloatDecimal(float value) {
+        return new PositiveHundredPercentage(value, true);
+    }
+
+    public static PositiveHundredPercentage fromFloatDecimal(float value, boolean flowValidated) {
+        return new PositiveHundredPercentage(value, flowValidated);
+    }
+
+    public static PositiveHundredPercentage fromDoublePercentage(double value) {
+        return new PositiveHundredPercentage(value / 100.0, true);
+    }
+
+    public static PositiveHundredPercentage fromDoublePercentage(double value, boolean flowValidated) {
+        return new PositiveHundredPercentage(value / 100.0, flowValidated);
+    }
+
+    public static PositiveHundredPercentage createZero() {
+        return new PositiveHundredPercentage(0, true);
+    }
+
+    @Override
+    public PositiveHundredPercentage toPositive() {
+        return this;
+    }
+
     @Override
     public void add(Percentage percentage) {
         if (percentage.getValue() < 0) {
